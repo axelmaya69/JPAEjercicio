8b6f63fd7a76c6f4a88bdc40de3f89d36f5ed84b
axelmaya69 <axeldbmaya96@gmail.com>
2024-08-27 23:04:39 -0600

    agregando las nuevas funcionalidades

--- src/main/java/com/ormjpa/persistencia/ControladoraPersistencia.java
+++ src/main/java/com/ormjpa/persistencia/ControladoraPersistencia.java
@@ -6,5 +6,8 @@ package com.ormjpa.persistencia;
  * @author axelmaya
  */
 public class ControladoraPersistencia {
-      AlumnoJpaController alujpa = new AlumnoJpaController();
+    
+    //se crea un nuevo objeto cuyo en esta clase a partir del alumnoJpa
+    //y es su unica funcion.
+    AlumnoJpaController alujpa = new AlumnoJpaController();
 }
