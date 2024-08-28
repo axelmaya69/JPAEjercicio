ac74d805c52bce7db3ae8b09be813845c246e9e9
axelmaya69 <axeldbmaya96@gmail.com>
2024-08-27 23:06:25 -0600

    nuevos comentarios

--- src/main/java/com/ormjpa/Ormjpa.java
+++ src/main/java/com/ormjpa/Ormjpa.java
@@ -10,7 +10,10 @@ import com.ormjpa.persistencia.ControladoraPersistencia;
 public class Ormjpa {
 
     public static void main(String[] args) {
+        //se crea otro objeto a partir de la clase controladorapersistencia
+        //cuya funcion tambien es crear un objeto de alumnojpa para controlarlo
         ControladoraPersistencia controler = new ControladoraPersistencia();
+        //mensaje para probar tambien si funciona
         System.out.println("Hello World!");
     }
 }
