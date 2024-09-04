fdbb54f49f0b9524bbd2875e0147607d713259c3
axelmaya69 <axeldbmaya96@gmail.com>
2024-09-03 21:10:37 -0600

    No

--- src/main/java/com/ormjpa/logica/Controladora.java
+++ src/main/java/com/ormjpa/logica/Controladora.java
@@ -1,71 +1,64 @@
-
 package com.ormjpa.logica;
 
 import com.ormjpa.persistencia.ControladoraPersistencia;
 import java.util.ArrayList;
 
-/** 
+/**
  *
- * @author axelmaya
- * Creando clase controladora para hacer el crud de alumnos.
- * 
+ * @author axelmaya Creando clase controladora para hacer el crud de alumnos.
+ *
  */
 public class Controladora {
-    
+
     ControladoraPersistencia controlPer = new ControladoraPersistencia();
-    
+
     //creacion de metodo para recibir a alumno ya estructurado
-    public void crearAlumno(Alumno alu){
-        
+    public void crearAlumno(Alumno alu) {
+
         //se crea alumno a partir de esta instancia y siguienlo los 
         //warnings de la izquierda en la clase ControladoraPersistencia();
-        controlPer.createAlumno(alu);       
+        controlPer.createAlumno(alu);
     }
-    
+
     //creacion de metodo para borrar alumno
-    public void deleteAlumno(int id){
+    public void deleteAlumno(int id) {
         controlPer.deleteAlumno(id);
     }
-    
+
     //creando metodo para hacer un edit de alumno
-    public void updateAlumno(Alumno alu){
+    public void updateAlumno(Alumno alu) {
         controlPer.updateAlumno(alu);
-    }   
+    }
+
     //metodo para buscar alumno 
-    public Alumno bringAlumno(int id){
+    public Alumno bringAlumno(int id) {
         return controlPer.bringAlumno(id);
     }
+
     //traer a todos los alumnos
-    public ArrayList<Alumno> bringAlumnos(){
+    public ArrayList<Alumno> bringAlumnos() {
         return controlPer.bringAlumnos();
     }
-    
-    
-    //metodos para carrera
-    
-        
-    public void crearCarrera(Carrera carre){
-        controlPer.createCarrera(carre);       
-    }
-    
 
-    public void deleteCarrera(int id){
+    //metodos para carrera
+    public void crearCarrera(Carrera carre) {
+        controlPer.createCarrera(carre);
+    }
+
+    public void deleteCarrera(int id) {
         controlPer.deleteCarrera(id);
     }
-    
-    public void updateCarrera(Carrera carre){
+
+    public void updateCarrera(Carrera carre) {
         controlPer.updateCarrera(carre);
-    }   
-    
-    
-    public Carrera bringCarrera(int id){
+    }
+
+    public Carrera bringCarrera(int id) {
         return controlPer.bringCarrera(id);
     }
-    
-    public ArrayList<Carrera> bringCarreras(){
+
+    public ArrayList<Carrera> bringCarreras() {
         return controlPer.bringCarreras();
     }
-    
-    
-    
+
 }
