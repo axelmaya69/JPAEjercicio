e053d805c88bb801c998d1835f075b809090d5ac
axelmaya69 <axeldbmaya96@gmail.com>
2024-09-04 20:36:15 -0600

    haciendo mapeo

--- src/main/java/com/ormjpa/logica/Materia.java
+++ src/main/java/com/ormjpa/logica/Materia.java
@@ -2,6 +2,9 @@
 package com.ormjpa.logica;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  *
@@ -10,8 +13,52 @@ import javax.persistence.Entity;
 @Entity
 public class Materia {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
     private String nombre;
     private String tipo;
+
+    public Materia(int id, String nombre, String tipo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+    public Materia(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "id=" + id + ", nombre=" + nombre + ", tipo=" + tipo + '}';
+    }
+    
+    
+ 
+    
     
 }
