7060847f54d214a2ef6a88f20b0119d23ad6a648
axelmaya69 <axeldbmaya96@gmail.com>
2024-09-04 20:32:33 -0600

    agregando relacion oneMany

---
+++ src/main/java/com/ormjpa/logica/Materia.java
@@ -0,0 +1,17 @@
+ 
+package com.ormjpa.logica;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author axelmaya
+ */ 
+@Entity
+public class Materia {
+    
+    private int id;
+    private String nombre;
+    private String tipo;
+    
+}
